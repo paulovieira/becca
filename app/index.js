41fc38534c5e27b59ef188985f2f1a86f6ccb7a9
paulovieira <paulovieira@gmail.com>
2016-03-22 17:22:30 +0000

    first commit

---
+++ app/index.js
@@ -0,0 +1,23 @@
+var becca = [
+{
+	category: "Effectiveness",
+	criteria: "General",
+	explanation: "The extent to which the intended outcome(s) has (have) been achieved. In terms of preventing climate change damage (e.g. reducing impacts, reducing exposure, enhancing resilience or enhancing adaptive capacity, reduction in economic losses). A further specification might be to consider effectiveness also in terms of ‘cost-effectiveness’.",
+	wheight: 1,
+	grade: undefined
+},
+{
+	category: "Effectiveness",
+	criteria: "Relevance",
+	explanation: "The extent to which the adaptation measures addressed climate change impact.",
+	wheight: 1,
+	grade: undefined
+},
+{
+	category: "Effectiveness",
+	criteria: "Avoided damage",
+	explanation: "Portion of the targeted potential damages that could be avoided by implementing the adaptation measures. The portion of avoided damage might result in expected gross benefits (Example for measurement: Avoided damages in physical and economic metrics)",
+	wheight: 1,
+	grade: undefined
+}
+]
