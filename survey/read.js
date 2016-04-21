9e26e5dd9dd583701de8b4969d1eb27e62d06d5d
paulovieira <paulovieira@gmail.com>
2016-04-21 18:30:11 +0100

    surbey

--- survey/read.js
+++ survey/read.js
@@ -29,39 +29,154 @@ This data is available in sheet1, but also on sheet 5, 9, etc
 // the main array
 var becca = [];
 
+
+if(!process.argv[2]){
+	console.log("excel file is missing in the argument");
+	process.exit(1);
+}
+// the rows relative to the case-studies (4 to 19)
+var rowStart = 4, rowEnd = 20;
+//var rowStart = 5, rowEnd = 6;
 workbook.xlsx
-	.readFile("Survey_Assessment_26_09_2014.xlsx")
+	.readFile(process.argv[2])
     .then(function() {
 
-    	var sheet1 = workbook.getWorksheet(1);
+    	var workSheet1 = workbook.getWorksheet(1);
 
-    	// the rows relative to the case-studies (4 to 19)
-		var rows = _.range(4,6);
+		var rows = _.range(rowStart, rowEnd);
 
-		// the cells relative to the case-study and criteria names (11 to the end)
-		var cells = _.range(11, sheet1._columns.length + 1);
+		// the cells relative to the case-study and criteria names (11 to the end, except cell 12)
+		var cells = _.range(11, workSheet1._columns.length + 1);
+		cells.splice(1, 1);
 
 		rows.forEach(function(row){
 
 			// list of key-value pairs
-			var pairs = [];
+			var pairs = [], caseStudy = {};
+
+			caseStudy["_row"] = row;
+			caseStudy["rates"] = {};
 
 			cells.forEach(function(cell){
 
-				var pair = cleanData([
-					sheet1.getRow(3).getCell(cell).value,
-					sheet1.getRow(row).getCell(cell).value,
-				]);
+				var pair = [
+					String(workSheet1.getRow(3).getCell(cell).value),
+					String(workSheet1.getRow(row).getCell(cell).value)
+				];
 
-				pairs.push(pair);
+
+				if(cell>=12 && !_.contains(["1","2","3","4","5","n/a", "null"], pair[1])){
+					console.log("cell: ", cell);
+					console.log("pair: ", pair);
+					
+					throw new Error("Grade value is not correct");
+				}
+
+				pairs.push(cleanRates(pair, cell));
 			});
 
-			becca.push(_.object(pairs));
+			var obj = _.object(pairs);
+			for(var key in obj){
+
+				var array = key.split("-").map(s => s.trim());
+				if(array.length===1){
+					//caseStudy[key] = obj[key];
+					caseStudy["_name"] = obj[key];
+					continue;
+				}
+
+				var dimension = array.pop().toLowerCase();
+				var criteriaName = array.join(" - ").trim();
+				caseStudy["rates"][criteriaName] = caseStudy["rates"][criteriaName] || {};
+
+				if(!_.contains(['relevance', 'feasibility', 'availability of information'], dimension)){
+					throw new Error("Dimension should be one of: 'relevance', 'feasibility', 'availability of information'")
+				}
+
+				caseStudy["rates"][criteriaName][dimension] = obj[key];
+			}
+
+			// make sure that for all criteria we for the 3 dimensions
+			for(var key in caseStudy["rates"]){
+
+				var obj = caseStudy[key];
+				if(typeof obj === "object" && Object.keys(obj).length!==3){
+					throw new Error("The criteria does not have the 3 dimensions")
+				}
+			}
+
+			// finally, all is good, append the object to the main array
+			becca.push(caseStudy);
 		});
     })
+
+	// 2nd pass
 	.then(function(){
 
-		console.log(becca)
+
+		var sheetsMapping = {
+			"sectors": 1,
+			"impacts": 5,
+			"perspective": 9,
+			"integration": 13,
+			"approach": 17,
+			"orientation": 21,
+			"setting": 25
+		};
+
+		for(var sheet in sheetsMapping){
+
+	    	var workSheet = workbook.getWorksheet(sheetsMapping[sheet]);
+
+	    	// the rows relative to the case-studies (4 to 19)
+			var rows = _.range(rowStart, rowEnd);
+
+			// the cells relative to the contextual dimensions
+			var cells = _.range(1, 10);
+
+			rows.forEach(function(row){
+
+				// list of key-value pairs
+				var pairs = [];
+				var caseStudy = _.findWhere(becca, {_row: row});
+				if(!caseStudy){
+					throw new Error("case study not found");
+				}
+
+				cells.forEach(function(cell){
+
+					var pair = [
+						String(workSheet.getRow(3).getCell(cell).value),
+						String(workSheet.getRow(row).getCell(cell).value)
+					];
+
+					if(pair[1]==="null"){
+						pair[1] = "";
+					}
+
+					if(pair[1].indexOf("x")>=0){
+						pairs.push(pair);	
+					}
+				});
+
+				var obj = _.object(pairs);
+
+				// finally, all is good, add the object with the context dimensions properties to the case study
+				caseStudy.contextDimensions = caseStudy.contextDimensions || {};
+				caseStudy.contextDimensions[sheet] = Object.keys(obj);
+			});
+
+		}
+
+	})
+	.then(function(){
+
+		becca.forEach(o => {
+			console.log(o["_name"]);
+			console.log(o["contextDimensions"]);
+			console.log("\n\n");
+		})
+
 	})
     .catch(function(err){
     	throw err;
@@ -95,10 +210,38 @@ Example:
 */
 
 // clean the original data
-function cleanData(pair){
+function cleanRates(pair, cell){
 
 	if(pair[0]==='Case Study:'){
 		pair[0] = 'Case Study';
+		return pair;
+	}
+
+	if(pair[1]==='n/a'){
+		pair[1] = '0';
+		return pair;
+	}
+
+	if(pair[1]==='null'){
+		pair[1] = '0';
+		console.log(`warning: cell ${ cell} does not have a value`)
+		return pair;
+	}
+
+	var wrong, correct;
+
+	wrong = "_x000D_";
+	correct = "";
+	if(pair[0].indexOf(wrong) >= 0){
+		pair[0] = pair[0].replace(wrong, correct);
+		return pair;
+	}
+
+	wrong = "Coherence - Interactions (conflicts/synergies) with other measures";
+	correct = "Coherence - Integration with policy domains, programmes or projects";
+	if(_.contains([128, 129, 131, 132, 134, 135], cell)){
+		pair[0] = pair[0].replace(wrong, correct);	
+		return pair;
 	}
 
 	return pair;
